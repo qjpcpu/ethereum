6cf362200cc8ce683f4e89d2246320bf4069e307
JasonQu <qjpcpu@gmail.com>
2018-03-13 15:09:53 +0800

    get from

--- contracts/helper.go
+++ contracts/helper.go
@@ -3,10 +3,27 @@ package contracts
 import (
 	"context"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"regexp"
 )
 
-// 某个地址是否合约地址
+var regFrom = regexp.MustCompile(`From:\s+([^\s]+)`)
+
+type TransactionWithExtra struct {
+	*types.Transaction
+}
+
+func (tx *TransactionWithExtra) From() *common.Address {
+	arr := regFrom.FindStringSubmatch(tx.Transaction.String())
+	if len(arr) == 2 {
+		addr := common.HexToAddress(arr[1])
+		return &addr
+	}
+	return nil
+}
+
+// 某个地址是否合约
 func IsContract(conn *ethclient.Client, hexAddr string) bool {
 	code, err := conn.CodeAt(context.Background(), common.HexToAddress(hexAddr), nil)
 	return err == nil && len(code) > 0
--- contracts/helper_test.go
+++ contracts/helper_test.go
@@ -1,6 +1,8 @@
 package contracts
 
 import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"testing"
 )
@@ -14,5 +16,18 @@ func TestIsContract(t *testing.T) {
 	isCon := IsContract(conn, addr)
 	if !isCon {
 		t.Fatalf("%s should be contract", addr)
+	}
+}
+func TestFrom(t *testing.T) {
+	addr := `0x1ac95afe8008760b6c34221cffe0ea4b84e3a194712923c80c098d4c31ab0437`
+	conn, err := ethclient.Dial("https://api.myetherapi.com/eth")
+	if err != nil {
+		t.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	tx, _, err := conn.TransactionByHash(context.Background(), common.HexToHash(addr))
+	t.Log(tx)
+	txe := TransactionWithExtra{tx}
+	if from := txe.From(); from.Hex() != common.HexToAddress("0x2437b75ab0e43e2fe068c9833dc488723aa944ec").Hex() {
+		t.Fatalf("get from fail")
 	}
 }
