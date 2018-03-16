177768e36dd1ba954556931bc54059d707586584
JasonQu <qjpcpu@gmail.com>
2018-03-17 00:17:20 +0800

    speed up

--- stats/crawl.go
+++ stats/crawl.go
@@ -22,11 +22,12 @@ type ContractInfo struct {
 }
 
 type TransactionScanner struct {
-	mycontracts map[string]ContractInfo
-	conn        *ethclient.Client
-	listener    TxListener
-	mutex       *sync.Mutex
-	scanning    bool
+	mycontracts  map[string]ContractInfo
+	badcontracts map[string]struct{}
+	conn         *ethclient.Client
+	listener     TxListener
+	mutex        *sync.Mutex
+	scanning     bool
 }
 
 type TransferPacket struct {
@@ -45,11 +46,19 @@ type TransferRecord struct {
 }
 
 type TxListener interface {
+	QueryContractFromCache(string) (ContractInfo, bool)
+	SaveContractToCache(ContractInfo)
 	RecieveRecords(TransferPacket)
 	ScanDone(start_block *big.Int, end_block *big.Int)
 }
 
-type StatPrinter struct{}
+type StatPrinter struct {
+	cache map[string]ContractInfo
+}
+
+func NewStatPrinter() *StatPrinter {
+	return &StatPrinter{cache: make(map[string]ContractInfo)}
+}
 
 func GetScanner(rawurl string, lis TxListener) (*TransactionScanner, error) {
 	if conn, err := ethclient.Dial(rawurl); err != nil {
@@ -61,10 +70,11 @@ func GetScanner(rawurl string, lis TxListener) (*TransactionScanner, error) {
 
 func GetScannerByClient(conn *ethclient.Client, lis TxListener) *TransactionScanner {
 	return &TransactionScanner{
-		mycontracts: make(map[string]ContractInfo),
-		mutex:       &sync.Mutex{},
-		conn:        conn,
-		listener:    lis,
+		mycontracts:  make(map[string]ContractInfo),
+		badcontracts: make(map[string]struct{}),
+		mutex:        &sync.Mutex{},
+		conn:         conn,
+		listener:     lis,
 	}
 }
 
@@ -72,6 +82,18 @@ func (ts *TransactionScanner) Reset() {
 	ts.mutex.Lock()
 	ts.mycontracts = make(map[string]ContractInfo)
 	ts.mutex.Unlock()
+}
+
+func (ts *TransactionScanner) isSubscribeAll() bool {
+	return len(ts.mycontracts) == 0
+}
+
+func (ts *TransactionScanner) SubscribeAll() error {
+	if ts.scanning {
+		return errors.New("is running")
+	}
+	ts.Reset()
+	return nil
 }
 
 func (ts *TransactionScanner) Subscribe(contractAddrs ...string) error {
@@ -95,12 +117,20 @@ func (ts *TransactionScanner) Subscribe(contractAddrs ...string) error {
 		info.Address = strings.ToLower(addr.Hex())
 		info.Name, _ = token.Name(nil)
 		info.Symbol, _ = token.Symbol(nil)
-		totalSupply, _ := token.TotalSupply(nil)
+		totalSupply, err := token.TotalSupply(nil)
+		if err != nil {
+			return err
+		}
 		info.TotalSupply = totalSupply.String()
 		ts.mycontracts[info.Address] = info
 		log.Infof("subscribe %s %s|%s OK", contractAddr, info.Name, info.Symbol)
 	}
 	return nil
+}
+
+func (ts *TransactionScanner) isBadContract(addr string) bool {
+	_, ok := ts.badcontracts[strings.ToLower(addr)]
+	return ok
 }
 
 func (ts *TransactionScanner) GetSubscribes() []ContractInfo {
@@ -124,12 +154,38 @@ func (ts *TransactionScanner) SubscribeContracts(contractInfos ...ContractInfo) 
 	return nil
 }
 
+func (ts *TransactionScanner) getContractInfo(addr string) (ContractInfo, error) {
+	addr = strings.ToLower(addr)
+	local, ok := ts.listener.QueryContractFromCache(addr)
+	if ok {
+		log.Debugf("get contract info from local:%+v", local)
+		return local, nil
+	}
+	var info ContractInfo
+	var token *erc20.Token
+	token, err := erc20.NewToken(common.HexToAddress(addr), ts.conn)
+	if err != nil {
+		log.Errorf("query contract fail:%v", err)
+		return info, err
+	}
+	info.Address = addr
+	info.Name, _ = token.Name(nil)
+	info.Symbol, _ = token.Symbol(nil)
+	totalSupply, err := token.TotalSupply(nil)
+	if err != nil {
+		log.Debugf("%s is not erc20 contract", addr)
+		ts.badcontracts[addr] = struct{}{}
+		return info, err
+	}
+	info.TotalSupply = totalSupply.String()
+	ts.listener.SaveContractToCache(info)
+	log.Debugf("get contract info from remote:%+v", info)
+	return info, nil
+}
+
 func (ts *TransactionScanner) StartScan(start_block *big.Int, limit uint64) error {
 	if ts.scanning {
 		return errors.New("is running")
-	}
-	if len(ts.mycontracts) == 0 {
-		return errors.New("no contracts to scan")
 	}
 	ts.mutex.Lock()
 	ts.scanning = true
@@ -175,19 +231,44 @@ func (ts *TransactionScanner) StartScan(start_block *big.Int, limit uint64) erro
 			//是否合约创建交易
 			if txe.IsContractCreation() {
 				caddr := txe.ContractAddress()
-				if info, ok := ts.mycontracts[caddr.Hex()]; ok {
-					records = append(records, TransferRecord{
-						Contract:           info,
-						IsContractCreation: true,
-						TxHash:             strings.ToLower(tx.Hash().Hex()),
-						From:               strings.ToLower(txe.From().Hex()),
-						To:                 "",
-						Amount:             new(big.Int).SetInt64(0),
-					})
+				if ts.isBadContract(caddr.Hex()) {
+					continue
+				}
+				if ts.isSubscribeAll() {
+					if info, err := ts.getContractInfo(caddr.Hex()); err == nil {
+						records = append(records, TransferRecord{
+							Contract:           info,
+							IsContractCreation: true,
+							TxHash:             strings.ToLower(tx.Hash().Hex()),
+							From:               strings.ToLower(txe.From().Hex()),
+							To:                 "",
+							Amount:             new(big.Int).SetInt64(0),
+						})
+					}
+				} else {
+					if info, ok := ts.mycontracts[strings.ToLower(caddr.Hex())]; ok {
+						records = append(records, TransferRecord{
+							Contract:           info,
+							IsContractCreation: true,
+							TxHash:             strings.ToLower(tx.Hash().Hex()),
+							From:               strings.ToLower(txe.From().Hex()),
+							To:                 "",
+							Amount:             new(big.Int).SetInt64(0),
+						})
+					}
 				}
 			} else {
 				toAddr := txe.To()
-				info, ok := ts.mycontracts[toAddr.Hex()]
+				if ts.isBadContract(toAddr.Hex()) {
+					continue
+				}
+				info, ok := ts.mycontracts[strings.ToLower(toAddr.Hex())]
+				if !ok && ts.isSubscribeAll() {
+					if ci, err := ts.getContractInfo(toAddr.Hex()); err == nil {
+						ok = true
+						info = ci
+					}
+				}
 				if ok && erc20.IsTransferFunc(tx.Data()) {
 					to, amount, err := erc20.DecodeTransferData(tx.Data())
 					if err != nil {
@@ -219,12 +300,21 @@ func (ts *TransactionScanner) StartScan(start_block *big.Int, limit uint64) erro
 	return nil
 }
 
-func (s StatPrinter) RecieveRecords(p TransferPacket) {
+func (s *StatPrinter) RecieveRecords(p TransferPacket) {
 	log.Infof("recieved %d records of block %v", len(p.Records), p.BlockNumber)
 	for _, record := range p.Records {
 		log.Infof("%s: %s ==> %s %v", record.TxHash, record.From, record.To, record.Amount.String())
 	}
 }
 
-func (s StatPrinter) ScanDone(start, end *big.Int) {
+func (s *StatPrinter) ScanDone(start, end *big.Int) {
+}
+
+func (s *StatPrinter) QueryContractFromCache(addr string) (ContractInfo, bool) {
+	c, ok := s.cache[addr]
+	return c, ok
+}
+
+func (s *StatPrinter) SaveContractToCache(c ContractInfo) {
+	s.cache[c.Address] = c
 }
--- stats/crawl_test.go
+++ stats/crawl_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestScan(t *testing.T) {
-	log.SetLogLevel(log.INFO)
-	scanner, err := GetScanner("https://api.myetherapi.com/eth", StatPrinter{})
+	log.SetLogLevel(log.DEBUG)
+	scanner, err := GetScanner("https://api.myetherapi.com/eth", NewStatPrinter())
 	if err != nil {
 		t.Fatal(err)
 	}
-	scanner.Subscribe(`0x9a642d6b3368ddc662ca244badf32cda716005bc`)
+	scanner.SubscribeAll()
 	scanner.StartScan(big.NewInt(5243713), 1)
 }
