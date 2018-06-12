08d5a2872d3f395e3ca900b602b323f03449cb82
JasonQu <qjpcpu@gmail.com>
2018-06-12 17:11:54 +0800

    pendingtx

--- etherscan/etherscan_test.go
+++ etherscan/etherscan_test.go
@@ -5,14 +5,21 @@ import (
 )
 
 func TestPendingTx(t *testing.T) {
-	hashes, err := PendingTxsHash(Online, "0x504fe7e01baa1b84e9832b8d718ae23697a4c43f")
+	hashes, err := PendingTxs(Online, "0x504fe7e01baa1b84e9832b8d718ae23697a4c43f")
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(hashes)
-	hashes, err = PendingTxsHash(Online, "")
+	hashes, err = PendingTxs(Online, "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(hashes)
+}
+func TestPendingDetail(t *testing.T) {
+	detail, err := GetBlockedPendingTx(Online, `0xe35f3e2a93322b61e5d8931f806ff38f4a4f4d88`, 83)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", detail)
 }
--- etherscan/pending_tx.go
+++ etherscan/pending_tx.go
@@ -1,10 +1,13 @@
 package etherscan
 
 import (
+	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"net/http"
 	"net/http/cookiejar"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,6 +27,39 @@ const (
 	Online  Env = `https://etherscan.io`
 	Ropsten     = `https://ropsten.etherscan.io`
 )
+
+type PendingTx struct {
+	Hash     string `json:"hash"`
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Value    string `json:"value"`
+	GasLimit uint64 `json:"gas"`
+	GasPrice string `json:"gasPrice"`
+	Nonce    uint64 `json:"nonce"`
+}
+
+func GetBlockedPendingTx(env Env, owner string, pending_nonce uint64) (PendingTx, error) {
+	if owner == "" {
+		return PendingTx{}, errors.New("no owner")
+	}
+	hashes, err := PendingTxs(env, owner)
+	if err != nil {
+		return PendingTx{}, err
+	}
+	if len(hashes) == 0 {
+		return PendingTx{}, errors.New("no pending tx")
+	}
+	for _, hash := range hashes {
+		detail, err := PendingTxDetail(env, hash)
+		if err != nil {
+			return detail, err
+		}
+		if pending_nonce == 0 || pending_nonce == detail.Nonce {
+			return detail, nil
+		}
+	}
+	return PendingTx{}, errors.New("no pending tx")
+}
 
 func PendingTxs(env Env, owner string) ([]string, error) {
 	var uri string
@@ -51,4 +87,55 @@ func PendingTxs(env Env, owner string) ([]string, error) {
 		hashes = append(hashes, s.Text())
 	})
 	return hashes, nil
+}
+
+func PendingTxDetail(env Env, txhash string) (PendingTx, error) {
+	var detail PendingTx
+	uri := fmt.Sprintf("%s/tx/%s", env, txhash)
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return detail, err
+	}
+	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36`)
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return detail, err
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return detail, err
+	}
+	detail.Hash = txhash
+	for loop := true; loop; loop = false {
+		doc.Find(`.container a[href^="/address"]`).Each(func(i int, s *goquery.Selection) {
+			switch i {
+			case 0:
+				detail.From = s.Text()
+			case 1:
+				detail.To = s.Text()
+			}
+		})
+		detail.Value = trimEmpty(doc.Find(`.container span[title^="The amount of ETH to be transferred to the recipient"]`).First().Text())
+		limit := trimEmpty(doc.Find(`.container span[title^="The amount of GAS supplied for this transaction to happen"]`).First().Text())
+		detail.GasLimit, _ = strconv.ParseUint(limit, 10, 64)
+		detail.GasPrice = trimEmpty(doc.Find(`.container span[title^="The price offered to the miner to purchase this amount of GAS"]`).First().Text())
+		doc.Find(`.container div .col-sm-9.cbs`).Each(func(i int, s *goquery.Selection) {
+			if strings.Contains(s.Text(), "{Pending}") {
+				str := strings.Replace(s.Text(), "{Pending}", "", -1)
+				str = strings.Replace(str, "|", "", -1)
+				detail.Nonce, _ = strconv.ParseUint(trimEmpty(str), 10, 64)
+			}
+		})
+	}
+	if detail.Nonce == 0 {
+		return detail, errors.New("can't find tx nonce")
+	}
+	return detail, nil
+}
+
+func trimEmpty(raw string) string {
+	raw = strings.Replace(raw, "\n", "", -1)
+	raw = strings.Replace(raw, "\r", "", -1)
+	return strings.TrimSpace(raw)
 }
