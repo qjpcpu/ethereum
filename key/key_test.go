517607f1ef579cb6d564b698ab104319a7ef1752
JasonQu <qjpcpu@gmail.com>
2018-06-14 10:07:48 +0800

    add test

--- key/key_test.go
+++ key/key_test.go
@@ -23,3 +23,12 @@ func TestSignature(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestVerifySign(t *testing.T) {
+	from := "0xE35f3e2A93322b61e5D8931f806Ff38F4a4F4D88"
+	signHex := "0xbd5280c97dd1f069875a512364ca8470c0d928c4701b2a7a89c775478caf7ac670a908823fc75e65e400592d57e9649b2f1f57b4fd3980f02fe9a683e271a41c1b"
+	msg := []byte("Hello world")
+	if err := VerifySign(from, signHex, msg); err != nil {
+		t.Fatal(err)
+	}
+}
